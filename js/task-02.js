21e83d3031e3f631f5c4885d3f968427cc7a5b8d
Vadim Vladimirov <kurtonchik@gmail.com>
2021-10-16 00:02:39 +0300

    done task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const liEl = ingredients.map((el) => {
+  const element = document.createElement("li");
+  element.textContent = el;
+  element.classList.add("item");
+  return element;
+});
+
+ingredientsEl.append(...liEl);
