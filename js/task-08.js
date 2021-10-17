6bea2c5f6b017b008482c079332845ec95c810d8
Vadim Vladimirov <kurtonchik@gmail.com>
2021-10-17 11:10:06 +0300

    done task08-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector("form.login-form");
 
+loginForm.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (!email || !password) {
+    return alert("Все поля должны быть заполненны");
+  }
+  const formData = new FormData(event.currentTarget);
+  const userData = {};
+  formData.forEach((value, name) => {
+    console.log(name, ":", value);
+    userData[name] = value;
+  });
+  console.log(userData);
+  console.log({ email, password });
+
+  loginForm.reset();
+}
